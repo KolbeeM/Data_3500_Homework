b3c9b0b1690e00f9eb8150b11b6c0a997cdc4045
KolbeeM <A02370264@usu.edu>
2026-02-06 22:55:08 +0000

    final commit for hw3

--- hw3.py
+++ hw3.py
@@ -33,3 +33,97 @@ while div > 0:
 
     number %= div
     div //= 10
+"""
+3.11 (Miles Per Gallon) Drivers are concerned with the mileage obtained by their automobiles. One driver has kept track of several tankfuls of gasoline by recording miles driven and gallons used for each tankful. Develop a sentinel-controlled-repetition script that prompts the user to input the miles driven and gallons used for each tankful. The script should calculate and display the miles per gallon obtained for each tankful. After processing all input information, the script should calculate and display the combined miles per gallon obtained for all tankfuls (that is, total miles driven divided by total gallons used).
+Enter the gallons used (-1 to end): 12.8
+Enter the miles driven: 287
+The miles/gallon for this tank was 22.421875
+Enter the gallons used (-1 to end): 10.3
+Enter the miles driven: 200
+The miles/gallon for this tank was 19.417475
+Enter the gallons used (-1 to end): 5
+Enter the miles driven: 120
+The miles/gallon for this tank was 24.000000
+Enter the gallons used (-1 to end): -1
+The overall average miles/gallon was 21.601423
+"""
+
+miles = 0
+gallons = 0
+
+gallons_used = int(input("Enter the gallons used (-1 to end): "))
+while gallons_used != -1:
+    miles_used = int(input("Enter the miles driven: "))
+
+    mpg = miles_used / gallons_used
+    print("The mpg for this tank was", mpg)
+    miles += miles_used
+    gallons += gallons_used
+    print()
+    gallons_used = int(input("Enter the gallons used (-1 to end): "))
+if gallons != 0:
+    overall_mpg = miles / gallons
+    print("The overall average mpg was: ", overall_mpg)
+else:
+    print("enter data dummy.")
+
+"""
+3.12 (Palindromes) A palindrome is a number, word or text phrase that reads the same backwards or forwards. For example, each of the following five-digit integers is a palindrome: 12321, 55555, 45554 and 11611. Write a script that reads in a five-digit integer and determines whether it’s a palindrome. [Hint: Use the // and % operators to separate the number into its digits.]
+"""
+
+digits = int(input("Enter a 5 digit number: "))
+first_digit = digits // 10000
+last_digit = digits % 10
+
+second_digit = (digits // 1000) % 10
+fourth_digit = (digits // 10) % 10
+
+if first_digit == last_digit and second_digit == fourth_digit:
+    print("palindrome!!!!")
+else:
+    print("not palindrome!")
+
+
+"""
+3.14 (Challenge: Approximating the Mathematical Constant π) Write a script that computes the value of π from the following infinite series. Print a table that shows the value of π approximated by one term of this series, by two terms, by three terms, and so on. How many terms of this series do you have to use before you first get 3.14? 3.141? 3.1415? 3.14159?
+You do not need to approximate to 3.1415 and 3.14159. You can
+just approximate to 3.14 and 3.141, but please tell me at what iteration of the loop you see
+3.14 twice in a row, not just once. Same for 3.141, tell me at which iteration in the loop you
+3.141 twice in a row, not just once.
+Hint: your for loop shouldn’t have to go more than 3000 iterations.
+Another hint: The reason I updated this requirement is because people’s AWS has been
+crashing when you run a for loop above 10000. Setting it to 3000, should not crash the
+server, and be enough to allow you to approximate to 3.14 and 3.141.
+"""
+pi = 0
+denominator = 1
+
+pi_2 = 0
+pi_3 = 0
+look_314 = False
+look_3141 = False
+print("term\tPI Number")
+
+for i in range(3000):
+    if i % 2 != 0:
+        pi -= 4 / denominator
+    else:
+        pi += 4 / denominator
+        
+    denominator += 2
+
+    if i <= 10:
+        print(i, "\t", round(pi, 5))
+    pi_314 = str(round(pi, 2))
+    pi_3141 = str(round(pi, 3))
+
+    if pi_314 == "3.14" and pi_2 == "3.14" and not look_314:
+        print("3.14 is in a row at iteration", i + 1)
+        look_314 = True
+
+    if pi_3141 == "3.141" and pi_3 == "3.141" and not look_3141:
+        print("3.141 is in a row at iteration", i + 1)
+        look_3141 = True
+        
+    pi_2 = pi_314
+    pi_3 = pi_3141
